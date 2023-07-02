0a9fbe384236b2aa72fd8bce84fce3404132d1ef
Indira <indira.mariya@gmail.com>
2023-07-01 20:23:30 -0700

    add projects section

--- my-personal-website/src/App.js
+++ my-personal-website/src/App.js
@@ -6,6 +6,8 @@ import linkedinImage from './images/linkedin.png';
 import githubImage from './images/github.png';
 import emailImage from './images/email.png';
 import arenaImage from './images/arena.png';
+import aboutMe from './images/profile.jpeg';
+import star from './images/star3.png';
 
 function App() {
   useEffect(() => {
@@ -23,16 +25,35 @@ function App() {
 
   return (
     <div className="App">
-
-
       <header className="App-header">
+
+        {/* <AsciiImage file={star} /> */}
+
+        <div className="nav">
+          <a href="#home">Home</a>
+          <span>&#x2022;</span>
+          <br></br>
+          <br></br>
+          <a href="#about">About</a>
+          <span>&#x2022;</span>
+          <br></br>
+          <br></br>
+          <a href="#projects">Projects</a>
+          <span>&#x2022;</span>
+          <br></br>
+          <br></br>
+          <a href="#contact">Contact Me</a>
+          <span>&#x2022;</span>
+          <br></br>
+          <br></br>
+        </div>
+
         <div className="container">
           <h1>
             I'm <span className="auto-type"></span>
           </h1>
         </div>
       </header>
-      {/* <AsciiImage /> */}
 
       <main>
         <section id="home">
@@ -40,50 +61,87 @@ function App() {
           <p>Introduce yourself and highlight your skills and expertise here.</p>
         </section>
 
+        <div id="divider"> </div>
+
+        <h2 id="aboutHead">About Me</h2>
         <section id="about">
-          <h2>About Me</h2>
-          <p>Provide information about yourself, your background, and your experience.</p>
+
+          <AsciiImage file={aboutMe} />
+          <p>I'm a dedicated and passionate software engineer with a strong background in web development. I have a keen eye for detail and a drive for delivering high-quality, efficient, and user-friendly solutions. With several years of experience in the industry, I have worked on a wide range of projects, from small startups to large-scale enterprise applications. My expertise lies in front-end development, where I enjoy crafting intuitive user interfaces using modern web technologies. I thrive in collaborative environments and value teamwork, communication, and continuous learning. Outside of coding, I love exploring the outdoors, playing the guitar, and immersing myself in a good book.</p>
         </section>
 
+        <div id="divider"> </div>
+
         <section id="projects">
-          <h2>Projects</h2>
-          <div className="project">
+          <h1>Projects</h1>
+
+          <div class="project">
             <h3>Project 1</h3>
+            {/* <AsciiImage id="ascii" /> */}
+            <div id="filler"></div>
             <p>Description of the project.</p>
           </div>
-          <div className="project">
+
+          <div class="project">
             <h3>Project 2</h3>
+            <div id="filler"></div>
+            {/* <AsciiImage id="ascii" */}
             <p>Description of the project.</p>
           </div>
         </section>
+
+
+        <div id="divider"> </div>
 
         <section id="contact">
           <h2>Contact Me</h2>
           <p>Provide contact information and a contact form for visitors to reach out to you.</p>
+          <form action="/submit-form" method="POST">
+
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required/>
+            <br></br>
+            <br></br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required/>
+            <br></br>
+            <br></br>
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <br></br>
+            <br></br>
+            <button type="submit">Submit</button>
+            </form>
+
         </section>
 
+        <section id = "test">
+          <div>
+            
+          </div>
+        </section>
       </main>
 
 
-      <footer>
-        <a href="https://www.linkedin.com/in/indira-m/">
-          <img src={linkedinImage} alt="LinkedIn" style={{ width: '35px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
-        </a>
-        <a href="https://www.linkedin.com/in/indira-m/">
-          <img src={emailImage} alt="Email" style={{ width: '35px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
-        </a>
-        <a href="https://github.com/IndiraMariya">
-          <img src={githubImage} alt="Github" style={{ width: '35px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
-        </a>
-        <a href="https://github.com/IndiraMariya">
-          <img src={arenaImage} alt="Arena" style={{ width: '40px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
-        </a>
-        <p style={{ paddingBottom: '15px', }}>&copy; 2023 Indira Mariya</p>
-      </footer>
+          <footer>
+            <a href="https://www.linkedin.com/in/indira-m/">
+              <img src={linkedinImage} alt="LinkedIn" style={{ width: '35px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
+            </a>
+            <a href="https://www.linkedin.com/in/indira-m/">
+              <img src={emailImage} alt="Email" style={{ width: '35px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
+            </a>
+            <a href="https://github.com/IndiraMariya">
+              <img src={githubImage} alt="Github" style={{ width: '35px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
+            </a>
+            <a href="https://github.com/IndiraMariya">
+              <img src={arenaImage} alt="Arena" style={{ width: '40px', paddingTop: '10px', paddingLeft: '5px', paddingRight: '5px' }} />
+            </a>
+            <p style={{ paddingBottom: '15px', }}>&copy; 2023 Indira Mariya</p>
+          </footer>
 
 
-    </div>
-  );
+        </div>
+        );
 }
 
-export default App;
+        export default App;
--- my-personal-website/src/ascii_image.js
+++ my-personal-website/src/ascii_image.js
@@ -1,43 +1,11 @@
 import React, { useEffect, useRef } from 'react';
-import flyImage from './images/butt3.png';
 
-const AsciiImage = () => {
+const AsciiImage = ({ file }) => {
   const canvasRef = useRef(null);
   const asciiRef = useRef(null);
 
   useEffect(() => {
     const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
-    const getFontRatio = () => {
-      const pre = document.createElement('pre');
-      pre.style.display = 'inline';
-      pre.textContent = ' ';
-
-      document.body.appendChild(pre);
-      const { width, height } = pre.getBoundingClientRect();
-      document.body.removeChild(pre);
-
-      return height / width;
-    };
-
-    const convertToGrayScales = (context, width, height) => {
-      const imageData = context.getImageData(0, 0, width, height);
-      const grayScales = [];
-
-      for (let i = 0; i < imageData.data.length; i += 4) {
-        const r = imageData.data[i];
-        const g = imageData.data[i + 1];
-        const b = imageData.data[i + 2];
-
-        const grayScale = toGrayScale(r, g, b);
-        imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
-
-        grayScales.push(grayScale);
-      }
-
-      context.putImageData(imageData, 0, 0);
-
-      return grayScales;
-    };
 
     const MAXIMUM_WIDTH = 80;
     const MAXIMUM_HEIGHT = 80;
@@ -57,39 +25,6 @@ const AsciiImage = () => {
 
       return [rectifiedWidth, height];
     };
-    
-
-    const file = flyImage; // Replace with your predetermined file path
-
-    const image = new Image();
-    image.onload = () => {
-        console.log('Image loaded:', image.width, image.height);
-  
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        const asciiImage = asciiRef.current;
-  
-        const [width, height] = clampDimensions(image.width, image.height);
-        console.log('Clamped dimensions:', width, height);
-  
-        canvas.width = width;
-        canvas.height = height;
-  
-        context.drawImage(image, 0, 0, width, height);
-        const grayScales = convertToGrayScales(context, width, height);
-        console.log('Gray scales:', grayScales);
-  
-        drawAscii(grayScales, width, asciiImage);
-      };
-  
-      image.onerror = (error) => {
-        console.error('Image loading error:', error);
-      };
-  
-      image.src = file;
-
-    const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\' ';
-    const rampLength = grayRamp.length;
 
     const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
 
@@ -105,12 +40,76 @@ const AsciiImage = () => {
 
       asciiImage.textContent = ascii;
     };
-  }, []);
+
+    const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\' ';
+    const rampLength = grayRamp.length;
+
+    const getFontRatio = () => {
+      const pre = document.createElement('pre');
+      pre.style.display = 'inline';
+      pre.textContent = ' ';
+
+      document.body.appendChild(pre);
+      const { width, height } = pre.getBoundingClientRect();
+      document.body.removeChild(pre);
+
+      return height / width;
+    };
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const asciiImage = asciiRef.current;
+
+    const image = new Image();
+    image.onload = () => {
+      console.log('Image loaded:', image.width, image.height);
+
+      const [width, height] = clampDimensions(image.width, image.height);
+      console.log('Clamped dimensions:', width, height);
+
+      canvas.width = width;
+      canvas.height = height;
+
+      context.drawImage(image, 0, 0, width, height);
+      const grayScales = convertToGrayScales(context, width, height);
+      console.log('Gray scales:', grayScales);
+
+      drawAscii(grayScales, width, asciiImage);
+    };
+
+    image.onerror = error => {
+      console.error('Image loading error:', error);
+    };
+
+    image.src = file;
+  }, [file]);
+
+  const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
+
+  const convertToGrayScales = (context, width, height) => {
+    const imageData = context.getImageData(0, 0, width, height);
+    const grayScales = [];
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      const r = imageData.data[i];
+      const g = imageData.data[i + 1];
+      const b = imageData.data[i + 2];
+
+      const grayScale = toGrayScale(r, g, b);
+      imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+
+      grayScales.push(grayScale);
+    }
+
+    context.putImageData(imageData, 0, 0);
+
+    return grayScales;
+  };
 
   return (
-    <div>
-      <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
-      <pre ref={asciiRef}></pre>
+    <div className="ascii-image-container">
+      <canvas ref={canvasRef} className="ascii-image-canvas"></canvas>
+      <pre ref={asciiRef} className="ascii-image"></pre>
     </div>
   );
 };
